b94dfbf19ec81d15cd1c4ad4bc15de8e27daf569
karpss <ola.fam10@gmail.com>
2023-05-08 16:54:42 +0100

    implemented heatmap

--- src/pages/Heatmap/index.tsx
+++ src/pages/Heatmap/index.tsx
@@ -3,10 +3,115 @@ import React, { useCallback, useEffect, useMemo, useState } from "react";
 import { ResponsiveHeatMapCanvas } from "@nivo/heatmap";
 import { fetchGenePhenotypesData } from "../../api/fetchGenePhenotypesData";
 import {parseHeatmapData} from "../../util/parseHeatmapData";
+import "./index.css";
 
 const Heatmap = () => {
+
     const [isLoading, setIsLoading] = useState(true);
     const [heatmapData, setHeatmapData] = useState<any>(null);
+    const [formattedheatmapData, setFormattedheatmapData] = useState<any | null>(null);
+    const [page, setPage] = useState(0);
+    const [dataPerPage, setDataPerPage] = useState(20);
+    const [percentage, setPercentage] = useState<number>(0);
+    const [filter, setFilter] = useState<"genes_list" | "calc_percentage" | "phenotype">();
+    const [phenotype, setPhenotype] = useState<string[]>([]);
+    const [genes, setGenes] = useState<string[]>([]);
+
+   /// console.log("Data", heatmapData);
+
+
+    const memoizedFilters = useCallback(() => {
+
+        if (!heatmapData) return;
+        console.log("Hiiiii")
+        console.log("Datass", heatmapData);
+      
+        
+let filteredHeatmapData: ParsedHeatmapData[] =
+ [...heatmapData.gene_list];
+
+        //console.log("FD", filteredHeatmapData)
+
+        if (filter === "genes_list") {
+            filteredHeatmapData = filteredHeatmapData.filter(gene => {
+              if (genes.length === 0) {
+                return true;
+              }
+              return genes.some(accessionId => accessionId === gene.marker_accession_id);
+            });
+          }
+
+          if (filter === "calc_percentage" && percentage > 0) {
+            const topPercentage = Math.ceil((percentage / 100) * (heatmapData?.genes.length ?? 0));
+            filteredHeatmapData = filteredHeatmapData.length > topPercentage
+              ? filteredHeatmapData.sort((a, b) => b.total_phenotype_terms - a.total_phenotype_terms).slice(0, topPercentage)
+              : filteredHeatmapData;
+          }
+
+          if (filter === "phenotype") {
+            filteredHeatmapData = filteredHeatmapData
+              .map((phen) => {
+                if (phenotype.length === 0) {
+                  return phen;
+                }
+                return {
+                  ...phen,
+                  data: phen.data.filter((p:any) => phenotype.includes(p.top_level_phenotype_term_id)),
+                };
+              })
+              .filter((phene) => phene.data.length > 0);
+          }
+    
+          //console.log("Here", filteredHeatmapData)
+
+          setFormattedheatmapData({
+            ...heatmapData,
+            gene_list: filteredHeatmapData,
+          });
+
+          
+
+          
+          if (page * dataPerPage >= filteredHeatmapData.length) {
+            setPage(0);
+          }
+
+
+
+
+
+
+    
+
+      
+      
+    },[heatmapData,
+      genes,
+      phenotype,
+      filter,
+      percentage,
+      page,
+      dataPerPage,]);
+
+
+      const handlePageChange = (
+        e: React.ChangeEvent<HTMLSelectElement>
+      ) => {
+        setDataPerPage(Number(e.target.value));
+      };
+
+
+        useEffect(() => {
+        
+        memoizedFilters();
+
+      }, [genes,
+        phenotype,
+        filter,
+        percentage,
+    memoizedFilters]);
+
+
 
     useEffect(() => {
         setIsLoading(true);
@@ -17,15 +122,108 @@ const Heatmap = () => {
         });
       }, []);
 
+      console.log("Here", formattedheatmapData);  
+      
+      
+      const totalHeatMapPages = Math.ceil(
+        (formattedheatmapData?.gene_list.length ?? 0) / dataPerPage
+      );
 
-      console.log("Data", heatmapData);
 
+      const paginatedHeatmapData = useMemo(() => {
+        return formattedheatmapData?.gene_list.slice(
+          page * dataPerPage,
+          page * dataPerPage + dataPerPage
+        );
+      }, [page, formattedheatmapData, dataPerPage]);
 
 
 
 
   return (
-    <div>Heatmap</div>
+    <div>
+     
+     {isLoading && (
+          <div className="loading">
+            <p>Loading...</p>
+          </div>
+        )}
+        {paginatedHeatmapData && paginatedHeatmapData.length > 0 ? (
+          <div className="heatmap_container" >
+            <ResponsiveHeatMapCanvas
+              key={paginatedHeatmapData.length}
+              data={paginatedHeatmapData}
+              margin={{ top: 120, right: 90, bottom: 60, left: 90 }}
+              valueFormat=">-0.3s"
+              xOuterPadding={0.1}
+              xInnerPadding={0.1}
+              yOuterPadding={0.1}
+              yInnerPadding={0.1}
+              
+              axisTop={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: -25,
+                legend: "Phenotype System",
+                legendOffset: -110,
+                legendPosition: "middle",
+              }}
+              axisLeft={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: "Gene List",
+                legendPosition: "middle",
+                legendOffset: -72,
+              }}
+              axisRight={{
+                tickSize: 5,
+                tickPadding: 5,
+                tickRotation: 0,
+                legend: "Gene List",
+                legendPosition: "middle",
+                legendOffset: 70,
+              }}
+              colors={{
+                type: "sequential",
+                scheme: "blues",
+              }}
+              labelTextColor={{
+                from: "color",
+                modifiers: [["darker", 8]]
+              }}
+              
+              borderWidth={2}
+              enableLabels={true}
+              legends={[
+                {
+                  anchor: "bottom",
+                  translateX: 0,
+                  translateY: 30,
+                  length: 400,
+                  thickness: 8,
+                  direction: "row",
+                  tickPosition: "after",
+                  tickSize: 3,
+                  tickSpacing: 4,
+                  tickOverlap: false,
+                  tickFormat: ">-.2s",
+                  title: "Phentotype Count Label →",
+                  titleAlign: "start",
+                  titleOffset: 4,
+                },
+              ]}
+              annotations={[]}
+            />
+          </div>
+        ) : (
+          <p className="empty_message"> No Data Found!</p>
+        )}
+
+
+
+
+    </div>
   )
 }
 
---
+++ src/type.d.ts
@@ -0,0 +1,36 @@
+/*eslint-disable*/
+
+
+interface PhenotypeData  {
+    mp_term_id: string;
+    mp_term_name: string;
+  };
+
+  interface TopLevelPhenotypeTerm  {
+    top_level_mp_term_id: string;
+    top_level_mp_term_name: string;
+  };
+  
+  interface ParsedHeatmapData{
+    id: string;
+    total_phenotype_terms:number;
+    marker_accession_id: string;
+    data: HeatmapListData[];
+
+  }
+
+
+
+interface HeatmapListData {
+    x:string;
+    y: number | null;
+    xKey: string;
+   yKey: string;
+   index: number;
+   top_level_phenotype_term_id:string;
+   phenotype_terms?: PhenotypeData[]
+   procedures?: string[];
+   top_level_phenotype_term?: TopLevelPhenotype & { index: number };
+   gene_list?: ParsedHeatmapData
+  
+}
