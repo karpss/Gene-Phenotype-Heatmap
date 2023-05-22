0b2411b050b71d9c6185af5206df79bc1c26ce65
karpss <ola.fam10@gmail.com>
2023-05-22 10:06:01 +0100

    Minor Update

--- src/type.d.ts
+++ src/type.d.ts
@@ -1,18 +1,12 @@
-/*eslint-disable*/
-
-
-interface FormattedData{
-  gene_list:ParsedHeatmapData[];
-  genes_list_ranking: number[]
+interface FormattedData {
+  gene_list: ParsedHeatmapData[];
+  genes_list_ranking: number[];
   top_level_phenotype_terms: ({
     index: number;
   } & TopLevelPhenotypeTerm)[];
-
-
 }
 
-interface FilterProps{
-  
+interface FilterProps {
   genes: string[];
   setGenes: (genes: string[]) => void;
   onFilterChange: () => void;
@@ -22,52 +16,47 @@ interface FilterProps{
   setPercentage: (percentages: number) => void;
   page: number;
   setPage: (page: number) => void;
-  filter: "genes_list" | "phenotype" | "calc_percentage" | undefined;
-  setFilter: React.Dispatch<React.SetStateAction<"genes_list" | "phenotype" | "calc_percentage" | undefined>>;
+  filter: 'genes_list' | 'phenotype' | 'calc_percentage' | undefined;
+  setFilter: React.Dispatch<
+    React.SetStateAction<
+      'genes_list' | 'phenotype' | 'calc_percentage' | undefined
+    >
+  >;
   data: TransformedData | null;
-
-
-
 }
 
+interface PhenotypeData {
+  mp_term_id: string;
+  mp_term_name: string;
+}
 
-interface PhenotypeData  {
-    mp_term_id: string;
-    mp_term_name: string;
-  };
+interface TopLevelPhenotypeTerm {
+  top_level_mp_term_id: string;
+  top_level_mp_term_name: string;
+}
 
-  interface TopLevelPhenotypeTerm  {
-    top_level_mp_term_id: string;
-    top_level_mp_term_name: string;
-  };
-  
-  interface ParsedHeatmapData{
-    id: string;
-    total_phenotype_terms:number;
-    marker_accession_id: string;
-    data: HeatmapListData[];
-
-  }
-
-
+interface ParsedHeatmapData {
+  id: string;
+  total_phenotype_terms: number;
+  marker_accession_id: string;
+  data: HeatmapListData[];
+}
 
 interface HeatmapListData {
-    x:string;
-    y: number | null;
-    xKey: string;
-   yKey: string;
-   index: number;
-   top_level_phenotype_term_id:string;
-   phenotype_terms?: PhenotypeData[]
-   procedures?: string[];
-   top_level_phenotype_term?: TopLevelPhenotypeTerm & { index: number };
-   gene_list?: ParsedHeatmapData
-  
+  x: string;
+  y: number | null;
+  xKey: string;
+  yKey: string;
+  index: number;
+  top_level_phenotype_term_id: string;
+  phenotype_terms?: PhenotypeData[];
+  procedures?: string[];
+  top_level_phenotype_term?: TopLevelPhenotypeTerm & { index: number };
+  gene_list?: ParsedHeatmapData;
 }
 
 interface PaginationProps {
-page: number;
-setPage: React.Dispatch<React.SetStateAction<number>>;
-totalHeatMapPages:number;
-
+  page: number;
+  setPage: React.Dispatch<React.SetStateAction<number>>;
+  totalHeatMapPages: number;
 }
