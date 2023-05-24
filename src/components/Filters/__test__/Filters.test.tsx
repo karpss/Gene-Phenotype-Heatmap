3788a9e077ccd8957dcd5bd04b1d17cb1d91de3f
karpss <ola.fam10@gmail.com>
2023-05-24 15:18:51 +0100

    Some unit tests for Filters component

--- src/components/Filters/__test__/Filters.test.tsx
+++ src/components/Filters/__test__/Filters.test.tsx
@@ -1,25 +1,25 @@
 /* eslint-disable */
-//Work in progress...
+// Work in progress...
+import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import { describe, it, expect, vi } from 'vitest';
 import Filters from '..';
 
 describe('Filters', () => {
-  it('handles gene filter correctly', () => {
-    const setGenesMock = vi.fn();
-    const setFilterMock = vi.fn();
-    const onFilterChangeMock = vi.fn();
-    const setPhenotypeMock = vi.fn();
-    const setPercentageMock = vi.fn();
-    const setPageMock = vi.fn();
-    
+  const setGenesMock = vi.fn();
+  const setFilterMock = vi.fn();
+  const onFilterChangeMock = vi.fn();
+  const setPhenotypeMock = vi.fn();
+  const setPercentageMock = vi.fn();
+  const setPageMock = vi.fn();
 
-    const geneSelections = [
-      { value: 'gene1', label: 'Gene 1' },
-      { value: 'gene2', label: 'Gene 2' },
-    ];
+  const geneSelections = [
+    { value: 'gene1', label: 'Gene 1' },
+    { value: 'gene2', label: 'Gene 2' },
+  ];
 
-    const { getByLabelText } = render(
+  it('handles percentage filter change', () => {
+    render(
       <Filters
         genes={['gene1']}
         setGenes={setGenesMock}
@@ -29,23 +29,48 @@ describe('Filters', () => {
         phenotype={['phen1']}
         setPhenotype={setPhenotypeMock}
         percentage={30}
-       setPercentage={setPercentageMock}
-       setPage={setPageMock}
-       page={0}
-       filter="genes_list"
-       
+        setPercentage={setPercentageMock}
+        setPage={setPageMock}
+        page={0}
+        filter="genes_list"
       />
     );
 
-    fireEvent.change(getByLabelText('Filter by gene list:'), {
-      target: { value: 'gene2' },
-    });
+    const percentageFilterInput = screen.getByLabelText(
+      'Filter top 10% of the genes that have the highest phenotype count'
+    );
+    fireEvent.change(percentageFilterInput, { target: { value: '20' } });
 
-    expect(setGenesMock).toHaveBeenCalledWith(['gene1', 'gene2']);
-    expect(setFilterMock).toHaveBeenCalledWith('genes_list');
+    expect(setPercentageMock).toHaveBeenCalledWith(20);
     expect(onFilterChangeMock).toHaveBeenCalled();
   });
 
+  it('handles reset button click', () => {
+    render(
+      <Filters
+        genes={['gene1']}
+        setGenes={setGenesMock}
+        setFilter={setFilterMock}
+        onFilterChange={onFilterChangeMock}
+        data={{ gene_list: geneSelections }}
+        phenotype={['phen1']}
+        setPhenotype={setPhenotypeMock}
+        percentage={30}
+        setPercentage={setPercentageMock}
+        setPage={setPageMock}
+        page={0}
+        filter="genes_list"
+      />
+    );
 
+    const resetButton = screen.getByRole('button', { name: 'Reset Filter' });
+    fireEvent.click(resetButton);
 
+    expect(setPhenotypeMock).toHaveBeenCalledWith([]);
+    expect(setGenesMock).toHaveBeenCalledWith([]);
+    expect(setPercentageMock).toHaveBeenCalledWith(0);
+    expect(setFilterMock).toHaveBeenCalledWith(undefined);
+    expect(setPageMock).toHaveBeenCalledWith(0);
+    expect(onFilterChangeMock).toHaveBeenCalled();
+  });
 });
